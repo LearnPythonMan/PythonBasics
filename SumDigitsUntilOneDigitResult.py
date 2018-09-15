3c68b2bdc4d61a63284f4293a5b69107eb8f3b55
callcthulu <oleligdowicz@gmail.com>
2018-09-15 22:44:51 +0200

    rozwiązanie zadania

--- SumDigitsUntilOneDigitResult.py
+++ SumDigitsUntilOneDigitResult.py
@@ -6,3 +6,9 @@ def digital_root(n):
     return n
 
 print(digital_root(test))
+
+#def digital_root(n):
+ #   return n if n < 10 else digital_root(sum(map(int,str(n))))
+
+#def digital_root(n):
+  #return n%9 or n and 9
