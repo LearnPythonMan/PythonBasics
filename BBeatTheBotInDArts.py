1658831153ea3f3c1acce08c5329b0ea7a67d9b1
callcthulu <oleligdowicz@gmail.com>
2018-10-01 23:04:46 +0200

    g

---
+++ BBeatTheBotInDArts.py
@@ -0,0 +1,16 @@
+def get_coordinates(score):
+    results = {}
+    other_results = {}
+    if score > 60:
+        return ( #coordinates of 60)
+    else:
+        closest = 0
+        for result, coordinate in results:
+            if score == result:
+                return coordinate
+            elif: score > result > closest:
+                closest = result
+        if #closest- result exist in results
+            return #this result
+        else: #return value from other results if closest - result == key
+    return (0, 0)
